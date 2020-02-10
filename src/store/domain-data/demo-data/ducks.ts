b968405d9454a62963cf96aa9bc78fa05f9a99ab
Shenghan Chen <zzdjk6@gmail.com>
2020-02-10 23:50:43 +1300

    BUILD: Update dependencies

--- src/store/domain-data/demo-data/ducks.ts
+++ src/store/domain-data/demo-data/ducks.ts
@@ -1,6 +1,5 @@
-import { Dispatch } from 'redux';
-import { Action } from 'redux-actions';
-import { createThunkRoutine, dispatchRoutine } from 'redux-thunk-routine';
+import { Action } from "redux-actions";
+import { createThunkRoutine, createThunk } from "redux-thunk-routine";
 
 // State
 type State = {};
@@ -12,36 +11,30 @@ export const routine3 = createThunkRoutine<number>('DEMO_ROUTINE3');
 
 const wait = (ms: number) => new Promise(resolve => setTimeout(() => resolve(), ms));
 
-// Actions
-export const fetchData1 = (makeError: boolean = false) => async (dispatch: Dispatch) => {
-  return await dispatchRoutine(dispatch, routine1, async () => {
-    await wait(1000);
-    if (makeError) {
-      throw new Error('Fetch Data1 Error!');
-    }
-    return 1;
-  });
-};
+// Thunks
+export const fetchData1 = createThunk(routine1, async (makeError: boolean = false) => {
+  await wait(1000);
+  if (makeError) {
+    throw new Error('Fetch Data1 Error!');
+  }
+  return 1;
+});
 
-export const fetchData2 = (makeError: boolean = false) => async (dispatch: Dispatch) => {
-  return await dispatchRoutine(dispatch, routine2, async () => {
-    await wait(2000);
-    if (makeError) {
-      throw new Error('Fetch Data2 Error!');
-    }
-    return 2;
-  });
-};
+export const fetchData2 = createThunk(routine2, async (makeError: boolean = false) => {
+  await wait(2000);
+  if (makeError) {
+    throw new Error('Fetch Data2 Error!');
+  }
+  return 2;
+});
 
-export const fetchData3 = (makeError: boolean = false) => async (dispatch: Dispatch) => {
-  return await dispatchRoutine(dispatch, routine3, async () => {
-    await wait(3000);
-    if (makeError) {
-      throw new Error('Fetch Data3 Error!');
-    }
-    return 3;
-  });
-};
+export const fetchData3 = createThunk(routine3, async (makeError: boolean = false) => {
+  await wait(3000);
+  if (makeError) {
+    throw new Error('Fetch Data3 Error!');
+  }
+  return 3;
+});
 
 // Reducer
 export default (state: State = {}, action: Action<any>): State => {
