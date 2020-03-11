0cf3c2b86bd16e12a36c8c0f7c0867d7f5275ee5
Shenghan Chen <zzdjk6@gmail.com>
2020-03-12 11:54:38 +1300

    BUILD: Upgrade dependencies

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demo-global-loading-state/build/precache-manifest.8d9e5fd3a7183cd8a86f36284c3d3a2d.js"
+  "/demo-global-loading-state/build/precache-manifest.8a08c8cfad18cef845b5c2f8e90370b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demo-global-loading-state/build/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
